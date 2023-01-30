b38ed7bdfe5dd996abf6032d2e7ea1db569a7770
Umeshsharma0707 <umeshsharma07072003@gmail.com>
2023-01-30 12:26:10 +0530

    oops concepts of c++

---
+++ module_5_cout_c++.cpp
@@ -0,0 +1,10 @@
+//   module 5
+//  write a c++ program to print message "welcome to the top technologies"
+
+#include<iostream>
+
+main()
+{
+    using namespace std;
+    cout<<"welcome to the top technologies";
+}
